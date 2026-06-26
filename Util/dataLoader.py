82c73d03f808a552b6051559e2818970f228f33e
Apishrana <apishrana682@gmail.com>
2026-06-26 20:26:36 +0530

    data loader

--- Util/dataLoader.py
+++ Util/dataLoader.py
@@ -1,7 +1,26 @@
 import os
+import pandas as pd
+from rich import print
+from pathlib import Path
 
 
 class DataLoader:
     def __init__(self):
         self.DataPath = os.getenv("DATA_PATH")
-        print(self.DataPath)
+
+    def loadCsv(self):
+        try:
+            if not Path(self.DataPath + "trips.csv").exists():
+                raise "err"
+            trip = pd.read_csv(self.DataPath + "trips.csv")
+            return trip
+        except:
+            print("[italic bold blue]Initializing Data[italic bold blue/]")
+            self.createCsv()
+
+    def createCsv(self):
+        trip = pd.DataFrame(
+            columns=["tripName", "source", "destination", "days", "budget"]
+        )
+        trip.to_csv(self.DataPath + "trips.csv", index=False)
+        self.loadCsv()
--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from Util.dataLoader import DataLoader
 from dotenv import load_dotenv
+import pandas as pd
+
+from Util.dataLoader import DataLoader
 
 
 class Tracker:
     def __init__(self):
-        load_dotenv()
-        DataLoader()
+        self.dataLoader = DataLoader()
+        self.trips = self.dataLoader.loadCsv()
+
+    def run(self):
+        pass
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    app = Tracker()
+    app.run()
