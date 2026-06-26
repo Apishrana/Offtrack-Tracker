d74a6f02f2c9745a769ff842ec910dd04fadf5f4
Apishrana <apishrana682@gmail.com>
2026-06-26 18:41:17 +0530

    setup.sh

---
+++ setup.sh
@@ -0,0 +1,12 @@
+python3 -m venv venv
+if [[ "$OSTYPE" == "darwin"* ]] || [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    source venv/bin/activate
+elif [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    source venv/Scripts/activate
+else
+    echo "Unsupported OS"
+    exit 1
+fi
+pip install --upgrade pip
+pip install -r requirements.txt
+python main.py
